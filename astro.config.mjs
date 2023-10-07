59a81095792841b14d9f3dc5dd84f2f86773966c
Micah Halter <micah@mehalter.com>
2023-10-07 06:49:15 -0400

    chore: update site url and add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrodocs-v2.vercel.app",
+  site: "https://docs.astronvim.com",
   integrations: [
     starlight({
       title: "AstroNvim",
