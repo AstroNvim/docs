48c51a434e7f9d92586820f9d88f108308123926
Micah Halter <micah@mehalter.com>
2025-03-26 14:32:08 -0400

    fix: update for external deployment

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,10 @@
 import { defineCollection } from "astro:content";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
-import { docSearchI18nSchema } from "@astrojs/starlight-docsearch/schema";
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   i18n: defineCollection({
     type: "data",
-    schema: i18nSchema({ extend: docSearchI18nSchema() }),
+    schema: i18nSchema(),
   }),
 };
